d97f2f8728ff57a6de08dc1e47a0ee1a2287f530
sohilpandya <sohilpandya@me.com>
2016-02-17 17:03:30 +0000

    added rest of the code

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
+import Winner from './Winner';
+import Vote from './Vote';
+import * as actionCreators from '../action_creators';
 
-export default React.createClass({
-  getPair: function() {
-    return this.props.pair || [];
-  },
+export const Voting = React.createClass({
+  mixins: [PureRenderMixin],
   render: function() {
-    return <div className="voting">
-      {this.getPair().map(entry =>
-        <button key={entry}>
-          <h1>{entry} sohil</h1>
-        </button>
-      )}
+    return <div>
+      {this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} />}
     </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    hasVoted: state.get('hasVoted'),
+    winner: state.get('winner')
+  };
+}
+
+export const VotingContainer = connect(
+                                mapStateToProps,
+                                actionCreators
+                                )(Voting);
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,31 @@
 // import React from 'react';
 var React = require('react');
 import ReactDOM from 'react-dom';
-import Voting from './components/Voting';
+import Router, {Route} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import io from 'socket.io-client';
+import reducer from './reducer';
+import {setState} from './action_creators';
+import App from './components/App';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const store = createStore(reducer);
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch(setState(state))
+);
+
+const routes = <Route component={App}>
+  <Route path="/results" component={ResultsContainer} />
+  <Route path="/" component={VotingContainer} />
+</Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
